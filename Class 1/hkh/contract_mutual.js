c1925796cf4a3f8eb5ef6ad23f053d6dcfbfdef1
HKH185 <1853003208@qq.com>
2021-11-03 12:30:49 +0800

    hkh作业:修改了一些代码

--- Class 1/hkh/contract_mutual.js
+++ Class 1/hkh/contract_mutual.js
@@ -25,7 +25,7 @@ const getTokenBalance = async (tokenaddress, address) => {
     let result = await tokenContract.methods.balanceOf(address).call();
     //获得代币有多少位小数
     let decimals = await tokenContract.methods.decimals().call();
-    weiname = common_tool.getweiname(decimals);
+    var weiname = common_tool.getweiname(decimals);
     let tokenbalance = web3.utils.fromWei(result.toString(10), weiname);
 
     //获得代币的符号
@@ -65,7 +65,7 @@ var EthereumTx = require('ethereumjs-tx');
 const util = require('ethereumjs-util')
 var moment = require('moment');
 //2.获取input内容
-function swaptokeninput(wbnbadddress, toaddress, tokenamountIn, amountOut, tokenaddress, tokendecimals = 18,) {
+function swapEthToTokensInput(wbnbadddress, toaddress, tokenamountIn, amountOut, tokenaddress, tokendecimals = 18,) {
 
     weiname = common_tool.getweiname(tokendecimals);
 
@@ -77,12 +77,102 @@ function swaptokeninput(wbnbadddress, toaddress, tokenamountIn, amountOut, token
 
     var deadline = (DEADLINE).toString(10);
     console.log("inputbefore");
-    var input_type =  abis.pancake[0]
+    var input_type =  {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "amountOut",
+                "type": "uint256"
+            },
+            {
+                "internalType": "address[]",
+                "name": "path",
+                "type": "address[]"
+            },
+            {
+                "internalType": "address",
+                "name": "to",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "deadline",
+                "type": "uint256"
+            }
+        ],
+        "name": "swapETHForExactTokens",
+        "outputs": [
+            {
+                "internalType": "uint256[]",
+                "name": "amounts",
+                "type": "uint256[]"
+            }
+        ],
+        "stateMutability": "payable",
+        "type": "function"
+    }
     console.log(input_type);
     var input = web3.eth.abi.encodeFunctionCall(input_type, [ amountOutMin, path, toaddress, deadline]);
     console.log(input)
     return input;
 }
+function swapTokensToEthInput(payContractAddress, fromAddress, payNum, getNum, getContractAddress, decimals = 18,) {
+
+    var weiname = common_tool.getweiname(decimals);
+
+    var path = [ payContractAddress,getContractAddress];
+    var amountIn = web3.utils.toWei(payNum.toString(10), weiname);
+    var amountOutMin = web3.utils.toWei(getNum.toString(10), weiname);
+    const now = moment().unix();
+    const DEADLINE = now + 60 * 20; //往后延迟20分钟
+
+    var deadline = (DEADLINE).toString(10);
+    console.log("inputbefore");
+    var input_type =  {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "amountIn",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "amountOutMin",
+                "type": "uint256"
+            },
+            {
+                "internalType": "address[]",
+                "name": "path",
+                "type": "address[]"
+            },
+            {
+                "internalType": "address",
+                "name": "to",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "deadline",
+                "type": "uint256"
+            }
+        ],
+        "name": "swapExactTokensForETH",
+        "outputs": [
+            {
+                "internalType": "uint256[]",
+                "name": "amounts",
+                "type": "uint256[]"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+    console.log(input_type);
+    var input = web3.eth.abi.encodeFunctionCall(input_type, [amountIn, amountOutMin, path, fromAddress, deadline]);
+    console.log(input)
+    return input;
+}
+
 // 3.swap系列相关方法
 function getPriKey(prikeystring) {
 
@@ -123,7 +213,7 @@ function getEthRawTx(fromAddress, toAddress, input, nonceNum, privKey, gasPrice,
     return serializedTx;
 }
 // swap系列相关方法最后的方法
-const swap = async (prikeystring,tokenaddress,wbnbaddress,addresspancake) => {
+const swapEthToTokens = async (prikeystring,tokenaddress,wbnbaddress,addresspancake) => {
     prikeystring = prikeystring.replace("0x", "")
     //这里的privKey 是私钥
     var priKey = getPriKey(prikeystring);
@@ -142,8 +232,7 @@ const swap = async (prikeystring,tokenaddress,wbnbaddress,addresspancake) => {
     var rate = 57;
     var ntoken = nbnb * (100 - los) / 100 * rate;
 
-    // var wbnbaddress = "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c"; //0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c
-    // var addresspancake = "0x10ed43c718714eb63d5aa57b78b54704e256024e";
+
     //获得input 内容
     //创建交易执行智能合约
     var toAddress = addresspancake
@@ -155,7 +244,7 @@ const swap = async (prikeystring,tokenaddress,wbnbaddress,addresspancake) => {
     var gasPrice = web3.utils.toWei((5).toString(10), 'Gwei');
     //设置 gaslimit 为 420000
     var gaslimit = 420000
-    var input = swaptokeninput(wbnbaddress, fromAddress, nbnb, ntoken, tokenaddress, decimals)
+    var input = swapEthToTokensInput(wbnbaddress, fromAddress, nbnb, ntoken, tokenaddress, decimals)
     let reslut = await signTransaction(fromAddress, toAddress, input, nonceCnt, priKey, gasPrice, nbnb, gaslimit)
     if (reslut) {
         console.log("交易成功")
@@ -165,12 +254,54 @@ const swap = async (prikeystring,tokenaddress,wbnbaddress,addresspancake) => {
     }
 
 }
+// swap系列相关方法最后的方法
+const swapTokensToEth = async (priKey,payContractAddress,getContractAddress,pancakeAddress) => {
+    var prikeystring = priKey.replace("0x", "")
+    //这里的privKey 是私钥
+    var priKey = getPriKey(prikeystring);
+    //获得自己的地址
+    var fromAddress = "0x" + util.privateToAddress(priKey).toString('hex');
 
-const token_to_token = async(parameter_map)=>{
+    //要交换的tokenadrress
+    var tokenContract = new web3.eth.Contract(abis.erc20, getContractAddress);
+    //获得代币有多少位小数
+    let decimals = await tokenContract.methods.decimals().call();
+    // 设置交易滑点,直接调用合约可以设置100的滑点，这里设置50的滑点
+    var los = 50;
+    // 假设要购买5个BNB的tokenA
+    var payNum = 0.005;
+    //假设bnb 和 token的兑换比例是 1:1000
+    var rate = 57;
+    var getNum = payNum * (100 - los) / 100 * rate;
+
+    //获得input 内容
+    //创建交易执行智能合约
+    var toAddress = pancakeAddress
+    //获得下一次交易的数
+    var nonceCnt = await web3.eth.getTransactionCount(fromAddress);
+    //交易需要5个BNB
+    var payNum = web3.utils.toWei((payNum).toString(10), 'ether');
+    //设置gasprice 为 5G wei
+    var gasPrice = web3.utils.toWei((5).toString(10), 'Gwei');
+    //设置 gaslimit 为 420000
+    var gaslimit = 420000
+    var input = swapTokensToEthInput(payContractAddress, fromAddress, payNum, getNum, getContractAddress, decimals)
+    let reslut = await signTransaction(fromAddress, toAddress, input, nonceCnt, priKey, gasPrice, 0, gaslimit)
+    if (reslut) {
+        console.log("交易成功")
+    }
+    else {
+        console.log("交易失败")
+    }
+
+}
+const tokenToToken = async(parameter_map)=>{
+
     var prikeystring = parameter_map['prikeystring']
-    var tokenaddress = parameter_map['tokenaddress']
+    var adatokenaddress = parameter_map['adatokenaddress']
     var wbnbaddress = parameter_map['wbnbaddress']
-    var addresspancake = parameter_map['addresspancake']
+    var atomtokenaddress = parameter_map['atomtokenaddress']
+    var pancakeaddress = parameter_map['pancakeaddress']
 
     prikeystring = prikeystring.replace("0x", "")
     //这里的privKey 是私钥
@@ -178,25 +309,25 @@ const token_to_token = async(parameter_map)=>{
     //获得自己的地址
     var fromAddress = "0x" + util.privateToAddress(priKey).toString('hex');
     //要交换的tokenadrress
-    var tokenContract = new web3.eth.Contract(abis.erc20, tokenaddress);
+    var tokenContract = new web3.eth.Contract(abis.erc20, atomtokenaddress);
     //获得代币有多少位小数
     let decimals = await tokenContract.methods.decimals().call();
     //2.计算滑点
     // 设置交易滑点,直接调用合约可以设置100的滑点，这里设置50的滑点
     var los = parameter_map['los'];
     // 原币数量
-    var source_token_num = parameter_map['source_token_num'];
+    var pay_token_num = parameter_map['pay_token_num'];
     //兑换比率
-    var tokenToTokenRate = 260//parameter_map['tokenToTokenRate']
+    var tokenToTokenRate = parameter_map['tokenToTokenRate']
     // 获取的兑换币个数
-    var get_token_num =  source_token_num* (100 - los) / 100 * tokenToTokenRate;
+    var get_token_num =  (pay_token_num* (100 - los) / 100 * tokenToTokenRate).toFixed(12);
     var pancakeFun = parameter_map['pancakeFun']
 
     //获得下一次交易的数
     var nonceCnt = await web3.eth.getTransactionCount(fromAddress);
 
     //交易需要多少个原币：换算成区块链认识的整数
-    source_token_num = web3.utils.toWei((source_token_num).toString(10), 'ether');
+    pay_token_num = web3.utils.toWei((pay_token_num).toString(10), 'ether');
 
     //设置gasprice 为 5G wei
     var gasPrice = web3.utils.toWei((5).toString(10), 'Gwei');
@@ -205,9 +336,9 @@ const token_to_token = async(parameter_map)=>{
 
     //获取交易用的input
     //var input = swaptokeninput(wbnbaddress, fromAddress, source_token_num, get_token_num, tokenaddress, decimals)
-    var weiname = common_tool.getweiname(decimals);
+    var weiname = common_tool.getweiname(parseInt(decimals));
 
-    var path = [wbnbaddress,tokenaddress];
+    var path = [adatokenaddress,wbnbaddress,atomtokenaddress];
 
     var amountOutMin = web3.utils.toWei(get_token_num.toString(10), weiname);
     const now = moment().unix();
@@ -215,12 +346,50 @@ const token_to_token = async(parameter_map)=>{
 
     var deadline = (DEADLINE).toString(10);
     console.log("inputbefore");
-    var input_type =  abis.pancakeMap[pancakeFun]
+    var input_type =  {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "amountIn",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "amountOutMin",
+                "type": "uint256"
+            },
+            {
+                "internalType": "address[]",
+                "name": "path",
+                "type": "address[]"
+            },
+            {
+                "internalType": "address",
+                "name": "to",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "deadline",
+                "type": "uint256"
+            }
+        ],
+        "name": "swapExactTokensForTokens",
+        "outputs": [
+            {
+                "internalType": "uint256[]",
+                "name": "amounts",
+                "type": "uint256[]"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
     console.log(input_type);
-    var input = web3.eth.abi.encodeFunctionCall(input_type, [source_token_num, amountOutMin, path, addresspancake, deadline]);
+    var input = web3.eth.abi.encodeFunctionCall(input_type, [pay_token_num, amountOutMin, path, fromAddress, deadline]);
     console.log(input)
     //let reslut = await signTransaction(fromAddress, toAddress, input, nonceCnt, priKey, gasPrice, 0, gaslimit)
-    let reslut = await signTransaction(fromAddress, addresspancake, input, nonceCnt, priKey, gasPrice, source_token_num, gaslimit)
+    let reslut = await signTransaction(fromAddress, pancakeaddress, input, nonceCnt, priKey, gasPrice, 0, gaslimit)
     if (reslut) {
         console.log("交易成功")
     }
@@ -230,8 +399,8 @@ const token_to_token = async(parameter_map)=>{
 }
 
 
-const send = async (prikeystring,toAddress) => {
-    prikeystring = prikeystring.replace("0x", "")
+const send = async (prikeystr,toAddress) => {
+    var prikeystring = prikeystr.replace("0x", "")
     //这里的privKey 是私钥
     var priKey = getPriKey(prikeystring);
     //获得自己的地址
@@ -262,11 +431,64 @@ const send = async (prikeystring,toAddress) => {
 
 }
 
+const transferByAbi = async (prikeystr,payContractAddress,getAccountAddress,payNum) => {
+    var prikeystring = prikeystr.replace("0x", "")
+    //这里的privKey 是私钥
+    var priKey = getPriKey(prikeystring);
+    //获得自己的地址
+    var fromAddress = "0x" + util.privateToAddress(priKey).toString('hex');
+    //需要支付的币数量
+    var amountInNum = web3.utils.toWei((payNum).toString(10), 'ether');
+    var abiFunctionMap = {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "dst",
+                "type": "address"
+            },
+            {
+                "name": "wad",
+                "type": "uint256"
+            }
+        ],
+        "name": "transfer",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+    //获取input
+    var input = web3.eth.abi.encodeFunctionCall(abiFunctionMap,[getAccountAddress,amountInNum]);
+
+    //设置gasprice 为 5G wei
+    var gasPrice = web3.utils.toWei((5).toString(10), 'Gwei');
+    //设置 gaslimit 为 420000
+    var gaslimit = 420000
+
+    //获得下一次交易的数
+    console.log("发送地址是：" + fromAddress)
+    var nonceCnt = await web3.eth.getTransactionCount(fromAddress);
+    let reslut = await signTransaction(fromAddress, payContractAddress, input, nonceCnt, priKey, gasPrice, 0, gaslimit)
+    if (reslut) {
+        console.log("交易成功")
+    }
+    else {
+        console.log("交易失败")
+    }
+}
+
 module.exports = {
     "getBNBBalance":getBNBBalance,
     "getTokenBalance":getTokenBalance,
     "getTokenToTokenRate":getTokenToTokenRate,
-    "swap":swap,
-    "token_to_token":token_to_token,
-    "send":send
+    "swapEthToTokens":swapEthToTokens,
+    "swapTokensToEth":swapTokensToEth,
+    "tokenToToken":tokenToToken,
+    "send":send,
+    "transferByAbi":transferByAbi
 }
--- Class 1/hkh/jobs.js
+++ Class 1/hkh/jobs.js
@@ -9,51 +9,75 @@ function getTokenBalance(){
         contract_mutual.getTokenBalance(item.token_address,item.query_address);
     })
 }
+
+// 作业2：transfer
+function transferByAbi() {
+    var prikeystr= ""; //私钥
+    var payContractAddress="0x3ee2200efb3400fabb9aacf31297cbdd1d435d47"; //支付币的智能合约地址
+    var getAccountAddress="";//收款地址
+    var payNum = 3.5;
+    contract_mutual.transferByAbi(prikeystr,payContractAddress,getAccountAddress,payNum)
+}
+
 // 作业3：
 // 1.在pancake上用 bnb兑换其他token
-function bnb_to_tokens() {
+function swapEthToTokens() {
     var priKey=""; //私钥
-    // 0x3ee2200efb3400fabb9aacf31297cbdd1d435d47
+    //
     var tokenaddress="0x3ee2200efb3400fabb9aacf31297cbdd1d435d47";//(ada地址)要兑换的token合约地址
     var wbnbaddress="0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c";// wbnb代币的合约地址
     var addresspancake = "0x10ed43c718714eb63d5aa57b78b54704e256024e";//pancake的地址
-    contract_mutual.swap(priKey,tokenaddress,wbnbaddress,addresspancake);
+    contract_mutual.swapEthToTokens(priKey,tokenaddress,wbnbaddress,addresspancake);
 }
+
+// 1.在pancake上用 其他token兑换bnb
+function swapTokensToEth() {
+    var priKey=""; //私钥
+    // 0x3ee2200efb3400fabb9aacf31297cbdd1d435d47
+    var getContractAddress="0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c";//(ada地址)要兑换的token合约地址
+    var payContractAddress="0x3ee2200efb3400fabb9aacf31297cbdd1d435d47";// wbnb代币的合约地址
+    var pancakeAddress = "0x10ed43c718714eb63d5aa57b78b54704e256024e";//pancake的地址
+    contract_mutual.swapTokensToEth(priKey,payContractAddress,getContractAddress,pancakeAddress);
+}
+
 // 2.send
 function send() {
     var priKey = "" // 私钥
-    var toAddress = tokenaddress = "0x9dF37B1A1Ce5dfC0509489CdE0b8a0b6b6a52735";
+    var toAddress = tokenaddress = "";
     contract_mutual.send(priKey,toAddress)
 }
 // 3.token_to_token
-function token_to_token() {
+async function tokenToToken() {
     var parameter_map = {}
     parameter_map['prikeystring'] = "" // 私钥
-    parameter_map['tokenaddress'] = "0x3ee2200efb3400fabb9aacf31297cbdd1d435d47"
+    parameter_map['adatokenaddress'] = "0x3ee2200efb3400fabb9aacf31297cbdd1d435d47"
     parameter_map['wbnbaddress'] = "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c"
-    parameter_map['addresspancake'] = "0x10ed43c718714eb63d5aa57b78b54704e256024e"
-    parameter_map['los'] = 50
-    parameter_map['source_token_num'] = 0.0015 //wbnb
-    parameter_map['tokenToTokenRate'] = 266  //contract_mutual.getTokenToTokenRate("0x28415ff2c35b65b9e5c7de82126b4015ab9d031f","0x3ee2200efb3400fabb9aacf31297cbdd1d435d47","0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c");
+    parameter_map['atomtokenaddress'] = "0x0eb3a705fc54725037cc9e008bdede697f62f335"
+    parameter_map['pancakeaddress'] = "0x10ed43c718714eb63d5aa57b78b54704e256024e"
+    parameter_map['los'] = 2
+    parameter_map['pay_token_num'] = 0.0015 //ada
+    parameter_map['tokenToTokenRate'] = await contract_mutual.getTokenToTokenRate("0xba591bf624b363b49476c0d1d2dbb5a566512a6f","0x0eb3a705fc54725037cc9e008bdede697f62f335","0x3ee2200efb3400fabb9aacf31297cbdd1d435d47");
     parameter_map['pancakeFun'] = "swapExactTokensForTokens";
-    contract_mutual.token_to_token(parameter_map)
+    contract_mutual.tokenToToken(parameter_map)
 }
 
 const main = async () => {
-    //作业1：读取10个代币余额   (测试通过)
+    //作业1：读取10个代币余额
     //getTokenBalance()
 
 
-    //作业2：给账户转bnb    (测试通过)
-    //send()
-
+    //作业2：给账户转代币
+    //transferByAbi()
 
     //作业3：
-    //3.1.在pancake上用 bnb兑换其他token   (测试通过)
-    //bnb_to_tokens()
+    //3.1.在pancake上用 bnb兑换其他token
+    //swapEthToTokens()
 
-    //3.2.在pancake上用 token兑换其他token (唉：琢磨了很久还是没走通：主要是swapExactTokensForTokens这个方法的input信息的第一个参数没弄明白)
-    //token_to_token()
+    //3.1.在pancake上用 token兑换其他bnb
+    //swapTokensToEth()
+
+    //3.3.在pancake上用 token兑换其他token
+    //tokenToToken()
 
 }
 main()
